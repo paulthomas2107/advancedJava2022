0efba1b88438fb00d3d34ec7fb50c02430d812b7
Paul Thomas <paulthomas2107@gmail.com>
2022-08-31 13:00:48 +0100

    Initial commit

---
+++ src/main/java/com/pault/advjava/Color.java
@@ -0,0 +1,24 @@
+package com.pault.advjava;
+
+public enum Color {
+
+    RED("red"),
+    GREEN("green"),
+    BLUE("blue");
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    Color(String value) {
+        this.value = value;
+    }
+
+    private String value;
+
+}
+
---
+++ src/main/java/com/pault/advjava/Day.java
@@ -0,0 +1,12 @@
+package com.pault.advjava;
+
+public enum Day {
+
+    SUNDAY,
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY
+}
